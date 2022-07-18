6d97f3af3a489c415040823f98027599c078d3c4
HamsterForest <republic7788@gmail.com>
2022-07-18 14:10:49 +0900

    css적용_모듈화

--- main.js
+++ main.js
@@ -1,73 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-
-var template={
-    homepage:function(bloglist,schlist){
-        return `
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <meta charset="UTF-8">
-                <title>마음의 안식</title>
-            </head>
-            <body>
-                <h1><a href="/">마음의 안식</a></h1>
-                <h3>블로그</h3>
-                <ul>
-                    ${bloglist}
-                </ul>
-                <h3>소소한 인생 계획</h3>
-                <ul>
-                    ${schlist}
-                </ul>
-                <h2>Blog And Schedule Management</h2>
-                <p>
-                    우리는 마음의 안식을 얻기위해 다양한노력을 기울인다.
-                    나는 나의 생각을 정리하고, 좀 더 조밀한 삶을 살아가기위해 일상을 계획한다.
-                </p>
-            </body>
-            </html>
-        `;
-    },
-    subpage:function(bloglist,schlist,title,description){
-        return `
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <meta charset="UTF-8">
-                <title>마음의 안식</title>
-            </head>
-            <body>
-                <h1><a href="/">마음의 안식</a></h1>
-                <h3>블로그</h3>
-                <ul>
-                    ${bloglist}
-                </ul>
-                <h3>소소한 인생 계획</h3>
-                <ul>
-                    ${schlist}
-                </ul>
-                <h2>${title}</h2>
-                <p>
-                    ${description}
-                </p>
-            </body>
-            </html>
-        `;
-    },
-    listmaker:function(filelist){
-        //목록형성(블로그)
-        var list='<ul>';
-        var i=0;
-        while(i<filelist.length){
-            list+=`<li><a href=/?id=${filelist[i]}>${filelist[i]}</a></li>`;
-            i+=1;
-        }
-        list+='</ul>';
-        return list;
-    }
-}
+var template=require('./lib/template.js');
+var style=require('./lib/style.js')
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -80,7 +15,7 @@ var app = http.createServer(function(request,response){
                 fs.readdir('schedule',function(error,schlist){
                     _bloglist=template.listmaker(bloglist);
                     _schlist=template.listmaker(schlist);
-                    page=template.homepage(_bloglist,_schlist);
+                    page=template.homepage(_bloglist,_schlist,style.css());
 
                     response.writeHead(200);
                     response.end(page);
@@ -99,7 +34,7 @@ var app = http.createServer(function(request,response){
                         fs.readdir('schedule',function(error,schlist){
                             _bloglist=template.listmaker(bloglist);
                             _schlist=template.listmaker(schlist);
-                            page=template.subpage(_bloglist,_schlist,title,description);
+                            page=template.subpage(_bloglist,_schlist,title,description,style.css());
         
                             response.writeHead(200);
                             response.end(page);
@@ -115,7 +50,7 @@ var app = http.createServer(function(request,response){
                         fs.readdir('schedule',function(error,schlist){
                             _bloglist=template.listmaker(bloglist);
                             _schlist=template.listmaker(schlist);
-                            page=template.subpage(_bloglist,_schlist,title,description);
+                            page=template.subpage(_bloglist,_schlist,title,description,style.css());
         
                             response.writeHead(200);
                             response.end(page);
